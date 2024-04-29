8e69bd99478808b8cac5af8d7402a5bf48fd502c
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-04-29 11:48:14 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,17 @@ function App() {
   return (
     <main>
       {/* Player names */}
-   
+      <ol id="players">
+        <li>
+          <span className="player-name">player 1</span>
+          <span className="player-symbol">X</span>
+        </li>
+
+        <li>
+          <span className="player-name">player 2</span>
+          <span className="player-symbol">O</span>
+        </li>
+      </ol>
       {/* game board */}
     </main>
   );
