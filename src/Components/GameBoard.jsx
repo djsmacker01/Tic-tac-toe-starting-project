01658c8ca868e7f13cb66c8ec1b1cf118bf5a56a
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-04-30 18:15:25 +0100

    commit

--- src/Components/GameBoard.jsx
+++ src/Components/GameBoard.jsx
@@ -10,7 +10,7 @@ export default function GameBoard() {
         <ol id="game-board">
             {initialBoardGame.map((row, rowIndex) => <li key={rowIndex}>
                 <ol>
-                    {row.map((col, colIndex) => <li key={rowIndex}><button></button></li>)}
+                    {row.map((playerSymbol, colIndex) => <li key={colIndex}><button>{playerSymbol}</button></li>)}
              </ol>
          </li>)}
         </ol>
