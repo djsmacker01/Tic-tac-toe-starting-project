08c35048cf89daafa77e29b905730a0b554c0f80
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-04-30 18:11:50 +0100

    commit

--- src/Components/GameBoard.jsx
+++ src/Components/GameBoard.jsx
@@ -7,8 +7,12 @@ const initialBoardGame = [
 
 export default function GameBoard() {
     return (
-        <ol>
-
+        <ol id="game-board">
+            {initialBoardGame.map((row, rowIndex) => <li key={rowIndex}>
+                <ol>
+                    {row.map((col, colIndex) => <li key={rowIndex}><button></button></li>)}
+             </ol>
+         </li>)}
         </ol>
     )
 }
