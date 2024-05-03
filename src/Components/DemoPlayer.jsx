573fccaff12ac3a7fe87910e1096f1d74c938fa7
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-05-03 09:59:59 +0100

    commit

--- src/Components/DemoPlayer.jsx
+++ src/Components/DemoPlayer.jsx
@@ -1,13 +1,13 @@
-export default function DemoPlayer(){
-    return (
-      <>
-        <li>
-          <span className="player">
-           <span> Player name</span>
-            <span>player symbol</span>
-                </span>
-                <button>Edit</button>
-        </li>
-      </>
-    );
+export default function DemoPlayer() {
+  return (
+    <>
+      <li>
+        <span className="player">
+          <span> Player name</span>
+          <span>player symbol</span>
+        </span>
+        <button>Edit</button>
+      </li>
+    </>
+  );
 }
