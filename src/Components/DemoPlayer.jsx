00d81d2282580f858ae77e6945c31788b7283063
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-05-03 18:14:45 +0100

    commit

--- src/Components/DemoPlayer.jsx
+++ src/Components/DemoPlayer.jsx
@@ -1,16 +1,22 @@
 import { useState } from "react";
 export default function DemoPlayer({ initialName, symbols }) {
-  const [edit, setIsEditing] = useState(false)
+  const [isEditing, setIsEditing] = useState(false)
 
   function handleEditButton() {
     console.log('button clicked')
     setIsEditing(true)
   }
+          
+  let playerName = <span className="player-name"> {initialName}</span>;
+  if (isEditing) {
+  playerName = <input type="text" />
+}
+  
   return (
     <>
       <li>
         <span className="player">
-          <span className="player-name"> {initialName}</span>
+          {playerName}
           <span className="player-symbol">{symbols}</span>
         </span>
         <button onClick={handleEditButton}>Edit</button>
