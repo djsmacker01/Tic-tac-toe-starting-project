52e60278bf809d29c170d79b12911eeaa4288030
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-05-05 16:22:37 +0100

    commit

--- src/Components/DemoPlayer.jsx
+++ src/Components/DemoPlayer.jsx
@@ -4,7 +4,7 @@ export default function DemoPlayer({ initialName, symbols }) {
 
   function handleEditButton() {
     console.log("button clicked");
-    setIsEditing(!isEditing);
+    setIsEditing((editing)=>!editing);
   }
 
   let playerName = <span className="player-name"> {initialName}</span>;
