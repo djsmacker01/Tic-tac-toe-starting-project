3d7494a6ffbe8862c9877482f7adc66959dd6479
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-05-08 10:06:30 +0100

    commit

--- src/Components/GameBoard.jsx
+++ src/Components/GameBoard.jsx
@@ -1,42 +1,43 @@
-// const initialBoardGame = [
-//   [null, null, null],
-//   [null, null, null],
-//   [null, null, null],
-// ];
-
-// export default function GameBoard() {
-//   return (
-//     <ol id="game-board">
-//       {initialBoardGame.map((row, rowIndex) => (
-//         <li key={rowIndex}>
-//           <ol>
-//             {row.map((playerSymbol, colIndex) => (
-//               <li key={colIndex}>
-//                 <button>{playerSymbol}</button>
-//               </li>
-//             ))}
-//           </ol>
-//         </li>
-//       ))}
-//     </ol>
-//   );
-// }
 const initialBoardGame = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
-//   [null, null, null],
 ];
 
 export default function GameBoard() {
-
-    return (
-        <ol id="game-board">
-            {initialBoardGame.map((row, rowIndex) => <li key={rowIndex}>
-                <ol>
-                    {row.map((col, colIndex) => <li key={colIndex}><button>{col}</button></li>)}
-                </ol>
-            </li>)}
-       </ol>
-    )
+  return (
+    <ol id="game-board">
+      {initialBoardGame.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button>{playerSymbol}</button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
 }
+
+// const initialBoardGame = [
+//   [null, null, null],
+//   [null, null, null],
+//   [null, null, null],
+// //   [null, null, null],
+// ];
+
+// export default function GameBoard() {
+
+//     return (
+//         <ol id="game-board">
+//             {initialBoardGame.map((row, rowIndex) => <li key={rowIndex}>
+//                 <ol>
+//                     {row.map((col, colIndex) => <li key={colIndex}><button>{col}</button></li>)}
+//                 </ol>
+//             </li>)}
+//        </ol>
+//     )
+// }
